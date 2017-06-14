ab0cf4e3c36ce0a616f42d65a7b821ed0cd7e3e6
Pocket <1178699765@qq.com>
2017-06-14 22:26:40 +0800

    下载链接

--- VLC-Demo/ViewController.m
+++ VLC-Demo/ViewController.m
@@ -15,6 +15,13 @@
 
 @implementation ViewController
 
+
+/**
+  MobileVLCKit.framework库因为过大并没有上传，可以自行下载或者用我提供的链接
+  下载好拖放进VLVPlayer工程目录下即可
+   下载链接：http://pan.baidu.com/s/1pLiBQKb
+ */
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
