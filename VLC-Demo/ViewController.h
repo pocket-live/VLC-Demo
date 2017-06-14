65395c9273b3c908c04d98ab6b27af7c288cf312
Pocket <1178699765@qq.com>
2017-06-14 21:00:16 +0800

    Initial Commit

---
+++ VLC-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VLC-Demo
+//
+//  Created by 刘茂平 on 2017/6/14.
+//  Copyright © 2017年 刘茂平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VLC-Demo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VLC-Demo
+//
+//  Created by 刘茂平 on 2017/6/14.
+//  Copyright © 2017年 刘茂平. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
